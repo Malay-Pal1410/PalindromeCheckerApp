d5027e2b2ea26d37c8c18faa09ac4c38ac29428c
Malay-Pal1410 <malaypal954@gmail.com>
2026-03-03 10:15:48 +0530

    UC3:Reverse String Based Palindrome Check

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -1,16 +1,43 @@
+/**
+ * Use Case 3: Reverse String Based Palindrome Check
+ *
+ * Description:
+ * This class checks whether a string is a palindrome
+ * by reversing the string and comparing it with
+ * the original value.
+ *
+ * At this stage, the application:
+ * - Iterates the string in reverse order
+ * - Builds a reversed version
+ * - Compares original and reversed strings
+ * - Displays the validation result
+ *
+ * This introduces transformation-based validation.
+ *
+ * @author Developer
+ * @version 3.0
+ */
 public class PalindromeApp {
 
-        public static void main(String[] args) {
+    /**
+     * Application entry point for UC3.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
 
-            System.out.println("=======================================");
-            System.out.println(" PALINDROME CHECKER APP ");
-            System.out.println("=======================================");
-            System.out.println("Use Case: UC1 - Application Entry & Welcome Message");
-            System.out.println("Version : 1.0.0");
-            System.out.println("Author : Malay Pal");
-            System.out.println("---------------------------------------");
-            System.out.println("Application started successfully!");
-            System.out.println("Ready to proceed to the next use case...");
-            System.out.println("=======================================");
+        String input = "madam"; // Hardcoded string for testing
+        String reversed = "";
+
+        // Iterate from the last character to the first.
+        for (int i = input.length() - 1; i >= 0; i--) {
+            // Using String Concatenation (+) to build the reversed string
+            reversed += input.charAt(i);
         }
+
+        // Using equals() Method to compare actual content
+        boolean isPalindrome = input.equals(reversed);
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: " + isPalindrome);
     }
+}
